e65000b39eae57c241742cd54e7b324527fc5405
Brian Emilius <be@rts.dk>
2024-03-22 13:52:34 +0100

    Add robots.txt and improve dialog close button

--- src/components/dialog/dialog-header.js
+++ src/components/dialog/dialog-header.js
@@ -6,7 +6,7 @@ export default function DialogHeader({ title, dialogRef }) {
 	return (
 		<header className="flex border-b border-gray-200 pb-2 mb-4">
 			<span className="text-xl font-semibold">{title}</span>
-			<button className="ml-auto" onClick={() => dialogRef.current.close()}><X /></button>
+			<button className="ml-auto" onClick={() => dialogRef.current.close()}><X /><span className="sr-only">Close</span></button>
 		</header>
 	)
 }
